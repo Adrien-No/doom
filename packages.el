a61ef34d8ee2f4af89304e7fe2f9a165bae0f668
adrien <adrien.no@proton.me>
2023-05-25 16:58:55 +0200

    Haskell installation

--- packages.el
+++ packages.el
@@ -48,3 +48,7 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+;; Haskell
+(package! haskell-mode)
+(package! lsp-haskell)
